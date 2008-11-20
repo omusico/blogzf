57e7cb1298846b2b80a7e08289c447463e3ef02c
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-11-20 17:47:16 +0000

    * Movemos la carpeta de layout dentro del webroot, para que pueda ser independiente. * Tambien agregamos un layout de wordpress colorpaper de ejemplos

---
+++ trunk/webroot/layout/colorpaper/frontend.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head profile="http://gmpg.org/xfn/11">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>My Web Blog  </title>
+	<script type="text/javascript" src="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/js/jquery-1.2.6.min.js"></script>
+	<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://freethemelayouts.com/blog/xmlrpc.php?rsd" />
+	<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://freethemelayouts.com/blog/wp-includes/wlwmanifest.xml" /> 
+	<!-- begin gallery scripts -->
+    <link rel="stylesheet" href="http://freethemelayouts.com/blog/wp-content/plugins/fcs/css/jd.gallery.css" type="text/css" media="screen" charset="utf-8"/>
+	<script type="text/javascript" src="http://freethemelayouts.com/blog/wp-content/plugins/fcs/scripts/mootools.v1.11.js"></script>
+	<script type="text/javascript" src="http://freethemelayouts.com/blog/wp-content/plugins/fcs/scripts/jd.gallery.js"></script>
+	<!-- end gallery scripts -->
+	<link rel="stylesheet" href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/styles/pink.css" type="text/css" />
+	<link rel="stylesheet" href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/style.css" type="text/css" />
+	<link rel="alternate" type="application/rss+xml" title="My Web Blog RSS Feed" href="http://freethemelayouts.com/blog/?feed=rss2" />
+	<link rel="pingback" href="http://freethemelayouts.com/blog/xmlrpc.php" />
+</head>
+<body>
+	<a name="top"></a>
+	<div id="footer-back"><div id="footer-bg">
+		<div id="main">
+			<div id="main-top">
+				<div id="navigationed">
+					<ul id="navigation" class="clearfix">
+						<li class="current_page_item"><a href="http://freethemelayouts.com/blog" title="My Web Blog">Home</a></li>
+						<li class="page_item page-item-2"><a href="http://freethemelayouts.com/blog/?page_id=2" title="About">About</a></li>
+                        <li class="page_item page-item-50"><a href="http://freethemelayouts.com/blog/?page_id=50" title="Contact">Contact</a></li>
+                        <li class="page_item page-item-53"><a href="http://freethemelayouts.com/blog/?page_id=53" title="Extras">Extras</a></li>
+					</ul>
+				</div>
+
+				<div id="container">
+					<div id="content-back" class="clearfix"><div id="content-bottom">
+						<div id="left-col">
+
+							<div class="clearfix">
+								<div id="colors">
+									<a href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/switcher.php?style=teal" class="color-blue">Blue</a>
+									<a href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/switcher.php?style=orange" class="color-orange">Orange</a>
+									<a href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/switcher.php?style=green" class="color-green">Green</a>
+									<a href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/switcher.php?style=pink" class="color-pink">Pink</a>
+									<a href="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/switcher.php?style=purple" class="color-purple">Purple</a>
+
+								</div>
+								<div id="featured">
+									<div class="featured-content">
+										<h3><a href="http://freethemelayouts.com/blog/?p=41" rel="bookmark" title="Permanent Link to Puppies mark birth of commercial pet cloning">Puppies mark birth of commercial pet cloning</a></h3>
+										<p>A U.S. woman received five puppies Tuesday that were cloned from her beloved late pitbull, becoming the inaugural customer of a South Korean company that says it is the world&#8217;s first successful commercial canine cloning service.</p>
+										<div class="post-meta">
+											<a href="http://freethemelayouts.com/blog/?p=41#more-41" rel="bookmark" title="Read More of Puppies mark birth of commercial pet cloning" class="btn btn-orange">Read More</a> <a href="http://freethemelayouts.com/blog/?p=41#comments" title="Comment on Puppies mark birth of commercial pet cloning">12 Comments</a> &nbsp; | &nbsp;  Posted by <span class="orange weight-normal">FTL</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="left-content">
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=39" rel="bookmark" title="Permanent Link to Ocean &#8216;dead zones&#8217; becoming global problem">Ocean &#8216;dead zones&#8217; becoming global problem</a></h4>
+
+									<p>Like a chronic disease spreading through the body, &#8220;dead zones&#8221; with too little oxygen for life are expanding in the world&#8217;s oceans.</p>
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=39#more-39" rel="bookmark" title="Read More of Ocean &#8216;dead zones&#8217; becoming global problem" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=39#comments" title="Comment on Ocean &#8216;dead zones&#8217; becoming global problem">1 Comment</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+
+									</div>
+								</div>
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=38" rel="bookmark" title="Permanent Link to Gulf &#8216;dead zone&#8217; suffocating fish and livelihoods">Gulf &#8216;dead zone&#8217; suffocating fish and livelihoods</a></h4>
+									<p>Fisherman Terry Pizani turns his captain&#8217;s wheel with a mournful expression on his face. Far below, the fishing grounds off the Louisiana coast where the 63-year-old has made a living for five decades have become an aquatic graveyard known as a &#8220;dead zone.&#8221;</p>
+
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=38#more-38" rel="bookmark" title="Read More of Gulf &#8216;dead zone&#8217; suffocating fish and livelihoods" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=38#comments" title="Comment on Gulf &#8216;dead zone&#8217; suffocating fish and livelihoods">2 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+									</div>
+								</div>
+
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=36" rel="bookmark" title="Permanent Link to Study: Organic food not more nutritional">Study: Organic food not more nutritional</a></h4>
+									<p>If you&#8217;ve ever found yourself in your local supermarket agonizing about whether the organic apples will be a more nutritional and greener choice than the cheaper non-organic ones, you&#8217;re probably not alone.</p>
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=36#more-36" rel="bookmark" title="Read More of Study: Organic food not more nutritional" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=36#respond" title="Comment on Study: Organic food not more nutritional">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+
+									</div>
+								</div>
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=34" rel="bookmark" title="Permanent Link to Eco communities: Living the green life">Eco communities: Living the green life</a></h4>
+									<p>The term &#8220;eco community&#8221; might conjure up an image of bearded hippies tending an allotment before sharing a mung bean stew. But as today&#8217;s urbanites become more concerned about reducing their carbon footprints, some are finding that modern eco communities offer them a way to live sustainably without foregoing their home comforts.</p>
+
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=34#more-34" rel="bookmark" title="Read More of Eco communities: Living the green life" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=34#respond" title="Comment on Eco communities: Living the green life">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+									</div>
+								</div>
+
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=32" rel="bookmark" title="Permanent Link to Arctic ice at second-lowest level ever">Arctic ice at second-lowest level ever</a></h4>
+									<p>New satellite measurements show that crucial sea ice in the Arctic Ocean has plummeted to its second-lowest level on record.</p>
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=32#more-32" rel="bookmark" title="Read More of Arctic ice at second-lowest level ever" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=32#respond" title="Comment on Arctic ice at second-lowest level ever">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+
+									</div>
+								</div>
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=30" rel="bookmark" title="Permanent Link to Oil price up on Russia tensions">Oil price up on Russia tensions</a></h4>
+									<p>Oil prices shot up more than $5 a barrel Thursday, rising to the highest level in over two weeks as escalating tensions with Russia stoked fears of supply disruptions to the West.</p>
+									<div class="post-meta">
+
+												<a href="http://freethemelayouts.com/blog/?p=30#more-30" rel="bookmark" title="Read More of Oil price up on Russia tensions" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=30#respond" title="Comment on Oil price up on Russia tensions">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+									</div>
+								</div>
+								
+														
+								<div class="post">
+
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=28" rel="bookmark" title="Permanent Link to Buffett: U.S. still in recession">Buffett: U.S. still in recession</a></h4>
+									<p>Billionaire investor Warren Buffett said Friday the U.S. economy continues to be in a recession, by his definition, and will continue to be for at least several more months.</p>
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=28#more-28" rel="bookmark" title="Read More of Buffett: U.S. still in recession" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=28#respond" title="Comment on Buffett: U.S. still in recession">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+
+									</div>
+								</div>
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=26" rel="bookmark" title="Permanent Link to Reports: Japan creating $73B stimulus plan">Reports: Japan creating $73B stimulus plan</a></h4>
+									<p>Japan is drawing up an economic stimulus package worth eight trillion yen ($73 billion) to help businesses and consumers cope with soaring fuel and commodity prices, news reports said Saturday.</p>
+									<div class="post-meta">
+
+												<a href="http://freethemelayouts.com/blog/?p=26#more-26" rel="bookmark" title="Read More of Reports: Japan creating $73B stimulus plan" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=26#respond" title="Comment on Reports: Japan creating $73B stimulus plan">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+									</div>
+								</div>
+								
+														
+								<div class="post">
+
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=24" rel="bookmark" title="Permanent Link to Mattel wins $100M in doll suit">Mattel wins $100M in doll suit</a></h4>
+									<p>A federal jury awarded Mattel Inc. $100 million in damages after a serious girl fight between the house of Barbie and the maker of her chief rival, the pouty-lipped Bratz dolls.</p>
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=24#more-24" rel="bookmark" title="Read More of Mattel wins $100M in doll suit" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=24#respond" title="Comment on Mattel wins $100M in doll suit">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+
+									</div>
+								</div>
+								
+														
+								<div class="post">
+									<div class="post-date"><span class="month block">Aug </span><span class="day">28 </span></div>
+									<h4><a href="http://freethemelayouts.com/blog/?p=23" rel="bookmark" title="Permanent Link to Durable goods post strong gains in June, July">Durable goods post strong gains in June, July</a></h4>
+									<p><span style="font-family: arial;">U.S. factories saw a surprisingly hefty increase in their orders for big-ticket products in July, reflecting continued strength in export sales and a boost to business investment from the government&#8217;s tax stimulus package.</span></p>
+
+									<div class="post-meta">
+												<a href="http://freethemelayouts.com/blog/?p=23#more-23" rel="bookmark" title="Read More of Durable goods post strong gains in June, July" class="btn btn-pink">Read More</a> <a href="http://freethemelayouts.com/blog/?p=23#respond" title="Comment on Durable goods post strong gains in June, July">0 Comments</a> &nbsp; | &nbsp;  Posted by <span class="pink weight-normal">FTL</span>
+									</div>
+								</div>
+
+								
+															
+							<div class="clearfix pagination bluegray">
+								<a href="http://freethemelayouts.com/blog/?paged=2">Previous Page</a> <span class="extralarge">1 of 3</span> 								</div>
+							</div>
+						</div>
+						<div id="right-col">
+																					<div id="logo">
+								<h1>My Web Blog</h1>
+
+							</div>
+							<ul id="right-content">
+																								<li><h5>About</h5>
+										About me. Edit this in the options panel.									</li>
+																
+																
+																<li id="flickr" class="clearfix"><h5>Photo Stream</h5>
+									<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=9&amp;display=latest&amp;size=s&amp;layout=x&amp;source=user&amp;user=87889801@N00"></script>
+								</li>
+								<li><h5>Categories</h5>
+									<ul>
+										<li class="cat-item cat-item-4"><a href="http://freethemelayouts.com/blog/?cat=4" title="World Business News &amp; More">Business</a></li>
+										<li class="cat-item cat-item-7"><a href="http://freethemelayouts.com/blog/?cat=7" title="Movies, Music, Games &amp; More">Entertainment</a></li>
+                                    	<li class="cat-item cat-item-5"><a href="http://freethemelayouts.com/blog/?cat=5" title="Science News">Science</a></li>
+                                    
+                                    	<li class="cat-item cat-item-3"><a href="http://freethemelayouts.com/blog/?cat=3" title="Topic about Technology">Technology</a></li>
+                                    	<li class="cat-item cat-item-1"><a href="http://freethemelayouts.com/blog/?cat=1" title="View all posts filed under World News">World News</a></li>
+									</ul>
+								</li>								
+								<li><h5>Recent Articles</h5>
+									<ul>
+										<li><a href="http://freethemelayouts.com/blog/?p=41">Puppies mark birth of commercial pet cloning</a></li><li><a href="http://freethemelayouts.com/blog/?p=39">Ocean &#8216;dead zones&#8217; becoming global problem</a></li><li><a href="http://freethemelayouts.com/blog/?p=38">Gulf &#8216;dead zone&#8217; suffocating fish and livelihoods</a></li><li><a href="http://freethemelayouts.com/blog/?p=36">Study: Organic food not more nutritional</a></li><li><a href="http://freethemelayouts.com/blog/?p=34">Eco communities: Living the green life</a></li><li><a href="http://freethemelayouts.com/blog/?p=32">Arctic ice at second-lowest level ever</a></li>
+									</ul>
+								</li>
+												
+
+								<li><h5>Archives</h5>
+									<ul>
+										<li><a href='http://freethemelayouts.com/blog/?m=200808' title='August 2008'>August 2008</a></li>
+									</ul>
+								</li>
+								
+								<li><h5>Search</h5>
+									<ul>
+				
+                                        <div class="box">
+                                        
+                                        <form method="get" id="searchform" action="http://freethemelayouts.com/blog/">
+                                        <div><input type="text" value="" name="s" id="s" class="text" size="40" /><br />
+                                        	<br />
+                                        <input type="submit" id="searchsubmit" value="Search" class="btn submit btn-green" />
+                                        </div>
+
+</form>
+
+					
+						</div>
+										
+    
+									</ul>
+
+								
+							</ul>
+<br /><a href="http://freethemelayouts.com/blog/?feed=rss2"><img src="http://freethemelayouts.com/blog/wp-content/themes/colorpaper/images/rss.gif" hspace="22" border="0" /></a>
+<br /><br /><br />						</div>
+					</div></div>
+					<div id="footer" class="clearfix">
+						<div>
+							<ul class="footer-pages clearfix">
+								<li class="first"><a href="http://freethemelayouts.com/blog" title="My Web Blog Home">Home</a></li>
+								<li class="page_item page-item-2"><a href="http://freethemelayouts.com/blog/?page_id=2" title="About">About</a></li>
+
+<li class="page_item page-item-50"><a href="http://freethemelayouts.com/blog/?page_id=50" title="Contact">Contact</a></li>
+<li class="page_item page-item-53"><a href="http://freethemelayouts.com/blog/?page_id=53" title="Extras">Extras</a></li>
+							</ul>
+							<br><p><big><strong>&copy; Copyright My Web Blog. All rights reserved.</strong></big><br>
+								Designed by FTL <a href="http://www.freethemelayouts.com/" style="color: #dce7c0;text-decoration: none;">Wordpress Themes</a> brought to you by <a href="http://smashingmagazine.com/" style="color: #dce7c0;text-decoration: none;">Smashing Magazine</a>
+
+								</p>
+						</div>
+						<a href="#top" class="btn btn-footer right">Back to Top</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div></div>
+
+	</body>
+</html>
