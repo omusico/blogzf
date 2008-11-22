cd22a4577959f1a7cacbcf1f95e21b25887aa79e
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-11-22 22:29:38 +0000

    Varios cambios, todos relacionados con el layout.
    Creamos un controller Sidebar, para que administre los menu de nuestra pagina.
    
    Y limpiamos el layout, para que quede mas prolijo, y se entienda mejor el uso de Zend_lAyout

---
+++ trunk/application/controller/SidebarController.php
@@ -0,0 +1,21 @@
+<?php 
+class SidebarController extends Zend_Controller_Action
+{
+	public function preDispatch()
+	{
+		$this->view->title = 'Blog ZF';	
+	}
+	public function menutopAction()
+	{
+		
+	}
+	public function rightcontentAction()
+	{
+	
+	}
+	public function footerAction()
+	{
+	}
+}
+
+
