6beec94407c2d166a3b735a11387ecf478d70267
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-12-02 13:50:24 +0000

    Refactoring de los cambios anteriores

--- trunk/application/controller/AdminController.php
+++ trunk/application/controller/AdminController.php
@@ -1,42 +1,8 @@
 <?php
-class AdminController extends Zend_Controller_Action
+class AdminController extends Blogzf_Controller 
 {
     public function preDispatch()
     {
-    	/**
-    	 * Traemo los datos del archivo de configuaracion
-    	 */
-    	$registry = Zend_Registry::getInstance();
-		$config = $registry->get( 'config_ini' );
-    	/**
-         * Url basicas del sistema
-         */
-        $this->view->staticServer = $config->site->static->server;
-        $this->view->appServer = $config->site->static->server;
-     	/**
-         * Agrego el titulo de la pagina
-         */
-        $this->view->headTitle()->append('Administracion de BlogZf');
-        /**
-         * Agrego los css para esta pagina
-         */
-        $this->view->headLink()
-            ->appendStylesheet( $this->view->staticServer . 'layout/admin/css/login.css' )
-            ->appendStylesheet( $this->view->staticServer . 'layout/admin/css/colors-fresh.css' );
-        /**
-         * Agrego los js basicos - POr ahora ninguno
-         */
-        /*
-         $this->view->headScript()
-			->appendFile( $this->view->staticServer . '/js/mootools/mootools.js');
-		 */
-         /**
-          * Asignamos a las diferentes vistas, su modulo correspondiente.
-          */
-//         $response = $this->getResponse();
-  //       $response->insert( 'sidebar', $this->view->action( 'rightcontent', 'sidebar' ));
-   //      $response->insert( 'footer', $this->view->action( 'footer', 'sidebar' ));
-     //    $response->insert( 'topMenu', $this->view->action( 'menutop','sidebar' ));
     }
     public function indexAction()
     {
--- trunk/library/Blogzf/Controller.php
+++ trunk/library/Blogzf/Controller.php
@@ -20,89 +20,50 @@ class Blogzf_Controller extends Zend_Controller_Action
     }
     public function init ()
     {
+    }
+    public function preDispatch ()
+    {
         /**
          * Configuracion de base de datos, archivos config y otros
          */
         $this->_config = new Blogzf_Config();
         $this->_config->load();
-        // El generic config debio configurar esto.
         $this->_db = Zend_Db_Table_Abstract::getDefaultAdapter();
-        if ($this->_config->getValue("metadata_cache", "conf", "enabled")) {
-            // First, set up the Cache
-            $frontendOptions = $this->_config->getValue("metadata_cache", "frontend");
-            $backendOptions = $this->_config->getValue("metadata_cache", "backend");
-            $cache = Zend_Cache::factory('Core', 'Sqlite', $frontendOptions, $backendOptions);
-            if ($this->_config->getValue("metadata_cache", "conf", "clean")) {
-                $cache->clean(Zend_Cache::CLEANING_MODE_ALL);
-            }
-            // Next, set the cache to be used with all table objects
-            Zend_Db_Table_Abstract::setDefaultMetadataCache($cache);
-        }
-        /**
-         * Configuracion general.
-         */
-        $this->layoutConfig();
-        $this->_session = new Zend_Session_Namespace();
         $this->_registry = Zend_Registry::getInstance();
-        $this->initView();
-    }
-    public function initView ()
-    {
-        parent::initView();
-        $this->view->staticServer = $this->_request->getBaseUrl();
-        $this->view->addBasePath('/', '');
-        // Setea el path de las vistas de cada modulo
-        $this->view->addScriptPath($this->_config->getValue('paths', 'scripts', 'helpers') . $this->getRequest()->getControllerName() . '/');
-        // Dojo
-        $this->view->addHelperPath('Zend/Dojo/View/Helper/', 'Zend_Dojo_View_Helper');
-        Zend_Dojo_View_Helper_Dojo::setUseDeclarative();
-    }
-    public function preDispatch ()
-    {
-        // if authentication is required make sure user
-        // has logged.
-        if ($this->_requiresAuth) {
-            $auth = Zend_Auth::getInstance();
-            if ($auth->hasIdentity()) {
-                // Identity exists; get it
-                $this->_user = $auth->getIdentity();
-            }
-            if ($this->_user == false) {
-                $this->_redirect('/user/login');
-            }
-            // add roles to class
-            if (count($this->_user->roles) > 0) {
-                foreach ($this->_user->roles as $role)
-                    $this->_roles[] = $role->name;
-            }
-        }
-        /**
-         * Seteamos el menu de la derecha
+        $this->layoutConfig();        
+    	/**
+    	 * Traemo los datos del archivo de configuaracion
+    	 */
+		$config = $this->_registry->get( 'config_ini' );
+    	/**
+         * Url basicas del sistema
          */
-        $layout = Zend_Layout::getMvcInstance();
-        //$layout->sidebar = $this->view->render( $this->_config->getValue( 'templates', 'sidebar', 'file' ) );
-    }
-    /**
-     * Averigua si el usuario tiene permisos o no para acceder a esta seccion
-     *
-     * @param $resource es el recurso al cual el usuario va a acceder
-     * @param $privilege si el usuario puede o no acceder a este recurso
-     * @uses isAllowed metodo de Zend_Acl
-     * @return true  o false, dependiendo si tiene o no permisos para acceder
-     */
-    public function allow ($resource, $privilege)
-    {
-        if (empty($this->_user)) {
-            return false;
-        }
-        $allowed = false;
-        foreach ($this->_roles as $role) {
-            if ($this->_acl->isAllowed($role, $resource, $privilege)) {
-                $allowed = true;
-                break;
-            }
-        }
-        return $allowed;
+        $this->view->staticServer = $config->site->static->server;
+        $this->view->appServer = $config->site->static->server;
+     	/**
+         * Agrego el titulo de la pagina
+         */
+        $this->view->headTitle()->append('Administracion de BlogZf');
+        /**
+         * Agrego los css para esta pagina
+         */
+        $this->view->headLink()
+            ->appendStylesheet( $this->view->staticServer . 'layout/admin/css/login.css' )
+            ->appendStylesheet( $this->view->staticServer . 'layout/admin/css/colors-fresh.css' );
+        /**
+         * Agrego los js basicos - POr ahora ninguno
+         */
+        /*
+         $this->view->headScript()
+			->appendFile( $this->view->staticServer . '/js/mootools/mootools.js');
+		 */
+         /**
+          * Asignamos a las diferentes vistas, su modulo correspondiente.
+          */
+         $response = $this->getResponse();
+         $response->insert( 'sidebar', $this->view->action( 'rightcontent', 'sidebar' ));
+         $response->insert( 'footer', $this->view->action( 'footer', 'sidebar' ));
+         $response->insert( 'topMenu', $this->view->action( 'menutop','sidebar' ));
     }
     /**
      * Configuramos los layout que tiene el sistema
