ea0864db1ca69b99161f12c105a2077a601495f1
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-12-03 13:26:47 +0000

    Dejamos un bootstrap mucho mas claro, limpio, y sobre todo sencillo, para no generar confusiones.

--- trunk/library/Blogzf/Config.php
+++ trunk/library/Blogzf/Config.php
@@ -5,41 +5,5 @@
  */
 class Blogzf_Generic_Config
 {
-    private $_config_sys;
-    private $_configName = '';
-    public function load ($configName = 'config.default.ini')
-    {
-        $this->_configName = $configName;
-        $this->_configSystem();
-        $this->_dataBaseConfig();
-    }
-    /**
-     * Configuración Base de Datos
-     */
-    private function _dataBaseConfig ()
-    {
-        $dataBase = Zend_Db::factory(
-            $this->_config_sys->database->db->adapter, 
-            $this->_config_sys->database->db->config->toArray());
-        Zend_Db_Table::setDefaultAdapter( $dataBase );
-        Zend_Registry::set( 'dbAdapter', $dataBase);
-    }
-    /**
-     * Configuración del sistema que será leída del config.ini y mergeada con 
-     * el archvo config.local.ini, este archivo no entra en el repositorio
-     * 
-     * @return config_sys 
-     */
-    private function _configSystem ()
-    {
-        $localConfig = new Zend_Config_Ini(self::LOCAL_FILE, NULL, true);
-        $this->_config_sys = new Zend_Config_Ini('../config/' . $this->_configName, NULL, true);
-        $this->_config_sys->merge( $localConfig )->setReadOnly();
-        /**
-         * Permite registra de forma pública las instancias de estas variables
-         * de configuración
-         */
-        Zend_Registry::set( 'config_sys', $this->_config_sys );
-        Zend_Registry::set( 'base_path', realpath('.') );
-    }
+
 }
--- trunk/webroot/index.php
+++ trunk/webroot/index.php
@@ -23,35 +23,10 @@ set_include_path(
 include "Zend/Loader.php";
 Zend_Loader::registerAutoload();
 /**
- * Configuramos el layout
- * Agregamos el layout de colorpaper
- */    
-$options = array(  
-	'layout' => 'admin/index',
-	'layout' => 'colorpaper/colorpaper',
-    'layoutPath' => 'layout/' );
-Zend_Layout::startMvc( $options );
-/**
- * Levantamos la configuracion del archivo config.default.ini
- */
-$config = new Zend_Config_Ini('config/config.default.ini');
-/**
- * Registramos de forma global la configuracion del sitio. 
- */
-$registry = Zend_Registry::getInstance();
-$registry->set( 'config_ini', $config );
-/**
  * Setup controller
  */
 $controller = Zend_Controller_Front::getInstance();
 $controller->setParam( 'config', 'config.default.ini' )
     ->setControllerDirectory('../application/controller')
     ->throwExceptions(true);
-
-$router = $controller->getRouter();
-$route = new Zend_Controller_Router_Route_Regex(
-    '^admin$',
-    array( 'controller' => 'admin', 'action' => 'index'  ));
-$router->addRoute('sitemap', $route);
-    
 $controller->dispatch();
