bdd4be3b1f45bb3a49639af2bb1dccf3a327e3ca
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-12-06 03:03:11 +0000

    Listo el CRUD de Post, y los podemos listar en la portada del blog * tambien agregamos una tabla y modelo Lov, con la lista de valores comunes en la primer carga pusimos los estado de una publicacion

--- application/admin/controller/PostsController.php
+++ application/admin/controller/PostsController.php
@@ -12,7 +12,7 @@ class Admin_PostsController extends Zend_Controller_Action
                     $post = $posts->createRow();
                     $post->title = $postData['title'];
                     $post->content = $postData['content'];
-                    $post->user_id = $postData['user_id'];
+                    $post->user_id = Zend_Auth::getInstance()->hasIdentity();
                     $post->comment = $postData['comment'];
                     $post->created_date = new Zend_Db_Expr('now()');
                     $post->status = $postData['status'];
@@ -46,10 +46,48 @@ class Admin_PostsController extends Zend_Controller_Action
 	}
 	public function updateAction()
 	{
-		
+        $form = new forms_Posts();
+        $posts = new Posts();
+        $postId = (int) $this->_request->getParam('postId', 0);
+        $post = $posts->find( $postId )->current();
+	    if ( $this->_request->isPost() ) {
+		    $postData = $this->_request->getPost();
+		    if ( $form->isValid( $postData ) ) {
+		        try {
+                    $post->title = $postData['title'];
+                    $post->content = $postData['content'];
+                    $post->user_id = Zend_Auth::getInstance()->hasIdentity();
+                    $post->comment = $postData['comment'];
+                    $post->created_date = new Zend_Db_Expr('now()');
+                    $post->status = $postData['status'];
+    		        $post->save();
+    		        $this->_redirect( '/admin/posts/read/' );
+		        } catch( Zend_Exception $e ) {
+		            echo "Caught exception: " . get_class( $e ) . "\n";
+                    echo "Message: " . $e->getMessage() . "\n";
+		        }
+		    } else {
+		        $form->populate( $postsData );
+		    }
+	    } else {
+            $form->populate( $post->toArray() );
+	    }
+	    
+	    $this->view->form = $form;		
 	}
 	public function deleteAction()
 	{
-		
+        $id = (int)$this->_request->getParam('id', 0);
+        $posts = new Posts();
+        $post = $posts->find( $id )->current();
+        if ( $post !== null ) {
+            try {
+                $post->delete();
+            } catch (Exception $e) {
+                echo "Caught exception: " . get_class( $e ) . "\n";
+                echo "Message: " . $e->getMessage() . "\n";
+            }
+        }
+        $this->_redirect('/admin/post/reader/');
 	}
 }
