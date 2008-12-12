39fbae9e56e00313721b9f5c0ebd2096dcfdf259
scramatte <scramatte@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-12-12 18:21:17 +0000

    - AuthorsController obsolete - Actualización modelo  Category a la nueva BD

--- application/model/Category.php
+++ application/model/Category.php
@@ -5,6 +5,6 @@ class Category extends Concentre_Db_Nested_Tree
     protected $_primary = 'category_id';
     protected $_left = 'category_left';
     protected $_right = 'category_right';
-    protected $_label = 'category_title';
-    protected $_url = 'category_url';
+    protected $_label = 'title';
+    protected $_url = 'url';
 }
