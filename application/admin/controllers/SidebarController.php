1024d7ab50786e5b0a57f4d670137360d8a16403
pablofmorales <pablofmorales@a44cdc68-b670-11dd-893d-d98698f66bbf>
2008-12-30 19:46:23 +0000

    Agregamos otro layout, para el admin, y un layout para el login

--- application/admin/controllers/SidebarController.php
+++ application/admin/controllers/SidebarController.php
@@ -4,21 +4,22 @@ class Admin_SidebarController extends Blogzf_Controller_Action
     public function preDispatch ()
     {}
     public function menutopAction ()
-    {
+    {$this->_helper->viewRenderer->setNoRender();
         /*if (! Zend_Auth::getInstance()->hasIdentity()) {
             $this->_helper->viewRenderer->setNoRender();
         }*/
     }
     public function rightcontentAction ()
     {
+        $this->_helper->viewRenderer->setNoRender();
        /** if (! Zend_Auth::getInstance()->hasIdentity()) {
             $this->_helper->viewRenderer->setNoRender();
         }*/
     }
     public function footerAction ()
-    {}
+    {$this->_helper->viewRenderer->setNoRender();}
     public function headerAction()
-    {
+    {$this->_helper->viewRenderer->setNoRender();
         /*if (! Zend_Auth::getInstance()->hasIdentity()) {
             $this->_helper->viewRenderer->setNoRender();
         } */       
